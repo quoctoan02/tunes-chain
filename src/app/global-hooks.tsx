37911d0ed8f2f3209a6df376862da084f232ffcb
PThangML <thangpv@moonlab.asia>
2023-04-14 00:48:53 +0700

    chore: example component

--- src/app/global-hooks.tsx
+++ src/app/global-hooks.tsx
@@ -1,4 +1,3 @@
-import { useSentryAuth } from '@hooks/core/useSentryAuth'
 import { useSentryTheme } from '@hooks/core/useSentryTheme'
 import { useSentryVersion } from '@hooks/core/useSentryVersion'
 import { useSentryWeb3 } from '@hooks/core/useSentryWeb3'
@@ -6,7 +5,7 @@ import { FC } from 'react'
 
 export const GlobalHooks: FC = () => {
   useSentryWeb3()
-  useSentryAuth()
+  // useSentryAuth()
   useSentryVersion()
   useSentryTheme()
   return null
---
+++ src/views/Example/TableExample.tsx
@@ -0,0 +1,9 @@
+import { FC } from 'react'
+
+interface TableExampleProps {}
+
+const TableExample: FC<TableExampleProps> = (props) => {
+  return <div>TableExample</div>
+}
+
+export default TableExample
