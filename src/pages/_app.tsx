285b91d597910e0db0a96ae9c89dfee92215f19e
PThangML <thangpv@moonlab.asia>
2023-04-26 13:59:46 +0700

    feat: update meta head in _app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,8 @@
 import { Provider } from '@app/provider'
-import { MetaHead } from '@components/Layout'
 import { Loader } from '@components/ui'
 import DefaultLayout from '@layouts/DefaultLayout'
 import { AppPropsWithLayout } from '@types'
+import Head from 'next/head'
 import { useEffect, useState } from 'react'
 import '../styles/styles.scss'
 
@@ -17,7 +17,24 @@ function MyApp({ Component, pageProps }: AppPropsWithLayout) {
 
   const App = (
     <>
-      <MetaHead />
+      <Head>
+        <title>Template NextJs</title>
+        <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
+
+        <meta itemProp="image" content={'/thumbnail.png'} />
+        <meta property="og:url" content={''} />
+        <meta property="og:description" content={''} />
+        <meta property="og:type" content="website" />
+        <meta property="og:image" content={'/thumbnail.png'} />
+
+        <meta itemProp="image" content={'/thumbnail.png'} />
+        <meta name="twitter:card" content="summary_large_image" />
+
+        <meta itemProp="image" content={'/thumbnail.png'} />
+        <meta charSet="UTF-8" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </Head>
       {getLayout(<Component {...pageProps} />)}
     </>
   )
